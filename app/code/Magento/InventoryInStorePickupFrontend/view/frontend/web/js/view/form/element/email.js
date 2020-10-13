1c63e8ed024cfe485a55acba72b7edaa8fd38a42
Mateusz Krzeszowiak <mateusz.krzeszowiak@creativestyle.pl>
2020-10-13 10:41:04 +0200

    Revert "Fix email validation error after customer login in checkout"
    This reverts commit 0e2c132f2d1e19cb9d8b7b465e362622c6647f28.

--- app/code/Magento/InventoryInStorePickupFrontend/view/frontend/web/js/view/form/element/email.js
+++ app/code/Magento/InventoryInStorePickupFrontend/view/frontend/web/js/view/form/element/email.js
@@ -3,15 +3,20 @@
  * See COPYING.txt for license details.
  */
 
-define(['Magento_Checkout/js/view/form/element/email', 'Magento_Checkout/js/checkout-data'], function(
-   Component,
-   checkoutData
+define(['jquery', 'Magento_Checkout/js/view/form/element/email'], function(
+    $,
+    Component
 ) {
     'use strict';
+
     return Component.extend({
         defaults: {
-            template: 'Magento_InventoryInStorePickupFrontend/form/element/email',
-            email: checkoutData.getInputFieldEmailValue()
+            template:
+                'Magento_InventoryInStorePickupFrontend/form/element/email',
+            links: {
+                email:
+                    'checkout.steps.shipping-step.shippingAddress.customer-email:email',
+            },
         },
     });
 });
